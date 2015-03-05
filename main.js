e6a00546c3547ce24c468d08d39e42aba3da4a8f
Iuliia Potapenko <julepka@gmail.com>
2015-03-04 21:21:07 -0800

    Update: header analyzing

--- main.js
+++ main.js
@@ -19,26 +19,46 @@ paypal.call('TransactionSearch', {StartDate: '2015-01-28T02:27:44.681Z'}, proces
 
 function processTransactions(error, transactions) {
     var trans = transactions;
+    //console.log(transactions);
     convert(trans);
 };
 
 function convert(data) {
     var table = [];
-    table.push(Object.keys(data[0]));
+    var headers = getHeader(data)
+    table.push(headers);
 
     for (var i = 0; i < data.length; ++i) {
         var trans = data[i];
-        var row = Object.keys(trans).map(function (key) {
-            return trans[key];
+        var row = headers.map(function (key) {
+            if (key in trans) {
+                return trans[key];
+            } else return "";
+
         });
         table.push(row);
     }
-    console.log(table);
+    //console.log(table);
 
     csv.writeToString(table, {
         headers: true
     }, function (err, data) {
         return console.log(data);
     });
+}
+
+function getHeader(data) {
+    var result = Object.keys(data[0]);
+    for (var i = 1; i < data.length; i++) {
+        var keys = Object.keys(data[i]);
+        for (var j = 0; j < keys.length; j++) {
+            if (result.indexOf(keys[j]) == -1) {
+                //result.push(keys[j]);
+                result.splice(j,0,keys[j]);
+            }
+
+        }
+    }
+    return result;
 }
 
